aa3eed1863b2f26531a883c6943563fc739938d2
nogamesense2 <45113201+nogamesense2@users.noreply.github.com>
2020-05-01 11:06:50 +0800

    Create string_ex_2 .py
    # TODO: print a log message using the variables above.

---
+++ string_ex_2 .py
@@ -0,0 +1,12 @@
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message using the variables above.
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+
+username_1 = "Yogesh"
+timestamp_1 = "16:20"
+url_1 = "http://petshop.com/pets/reptiles/pythons"
+print(username_1 + " " + "accessed the site" + " " + url_1 + " " + "at" + " " + timestamp_1)
