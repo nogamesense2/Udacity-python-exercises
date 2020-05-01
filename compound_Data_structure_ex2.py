4fc2d1cadd1052b9faa6d6284d29a23f0e1e8200
nogamesense2 <45113201+nogamesense2@users.noreply.github.com>
2020-05-01 17:08:21 +0800

    Update compound_Data_structure_ex2.py

--- compound_Data_structure_ex2.py
+++ compound_Data_structure_ex2.py
@@ -6,7 +6,7 @@ verse_list = verse.split()
 print(verse_list, '\n')
 
 # convert list to a data structure that stores unique elements
-verse_set = set(verse)
+verse_set = set(verse_list)
 print(verse_set, '\n')
 
 # print the number of unique words
