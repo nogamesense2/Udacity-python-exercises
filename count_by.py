5d2b5f3ffb8aa5fd030d52cd49aa021758682731
nogamesense2 <45113201+nogamesense2@users.noreply.github.com>
2020-05-02 22:14:02 +0800

    Create count_by.py

---
+++ count_by.py
@@ -0,0 +1,10 @@
+start_num = 5
+end_num = 10
+count_by = 1
+break_num = start_num
+# write a while loop that uses break_num as the ongoing number to
+#   check against end_num
+while(break_num < end_num):
+    break_num += count_by
+
+print(break_num)
