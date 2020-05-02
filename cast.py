ef2768ce3d8a2c0ec0080ff49383e6e8341e1389
nogamesense2 <45113201+nogamesense2@users.noreply.github.com>
2020-05-02 21:38:31 +0800

    Create cast.py

---
+++ cast.py
@@ -0,0 +1,8 @@
+cast = {
+           "Jerry Seinfeld": "Jerry Seinfeld",
+           "Julia Louis-Dreyfus": "Elaine Benes",
+           "Jason Alexander": "George Costanza",
+           "Michael Richards": "Cosmo Kramer"
+       }
+for key, value in cast.items():
+    print("Actor: {} Role: {}".format(key,value))
